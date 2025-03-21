f3b41ffc59f0cf23ea3b2d8ea15b7bd3e4fac747
deadflowers <deadflowers@users.noreply.github.com>
2025-03-21 11:10:23 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
-self.addEventListener('fetch', function(event) {
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('Service Worker registered'))
+    .catch(error => console.error('Service Worker registration failed:', error));
+  
+  self.addEventListener('fetch', function(event) {
   // it can be empty if you just want to load from cache
 });
